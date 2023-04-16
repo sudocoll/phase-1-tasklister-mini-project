366741139c40bd37800381be9a1f8bdf03078d4e
Collette Charles <collettecra123@gmail.com>
2023-04-16 16:29:24 -0400

    Complete Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addingEventListeners()
 });
+
+
+
+function addingEventListeners() {
+ document.getElementById('create-task-form')
+ .addEventListener('submit', handleFormSubmit);
+  
+}
+
+function handleFormSubmit(e){
+e.preventDefault()
+ 
+const task = e.target /* ['new-task-description'] */ [0].value
+const priorityLevel = parseInt(e.target[1].value);
+displayTask(task,priorityLevel)
+
+}
+
+function displayTask(task,priorityLevel){
+const taskUl = document.getElementById('tasks');
+const taskLi = document.createElement('li');
+
+taskLi.textContent = task + ' '
+taskLi.style.color = getPriorityColor(priorityLevel);
+taskUl.appendChild(taskLi);
+
+const deleteBtn = document.createElement('button');
+deleteBtn.textContent = "X";
+taskLi.appendChild(deleteBtn)
+ 
+deleteBtn.style.backgroundColor = "red"
+deleteBtn.style.color = "white"
+deleteBtn.style.fontSize = '10px'
+
+deleteBtn.addEventListener('click', deleteTask)
+
+
+}
+
+function deleteTask(e){
+ e.target.parentNode.remove();
+}
+
+function getPriorityColor(priorityLevel){
+
+if(priorityLevel === 1){
+  return 'red';
+}
+else if(priorityLevel === 2){
+  return 'green';
+}
+else{
+  return 'blue'
+}
+}
